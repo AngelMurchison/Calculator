2a61be2ac28f0e1fb9d563dff8bf0d8c6fc9d2ed
Angel Murchison <angelmurchison09@gmail.com>
2016-11-30 15:51:40 -0500

    Created xaml.cs logic for all operations, annotated logic, removed extra .cs file

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -26,39 +26,55 @@ namespace Calculator
             InitializeComponent();
         }
 
-        private void addition(object sender, RoutedEventArgs e)
+        private void additionLogic(object sender, RoutedEventArgs e)
+        {
+            int intput1 = 0;
+            int intput2 = 0;
+            // parsing user input from the 2 textboxes into integers.
+            int.TryParse(input1.Text, out intput1);
+            int.TryParse(input2.Text, out intput2);
+            // placing the result of the operation into the label Answer
+            Answer.Content = intput1 + intput2;
+        }
+
+        private void subtractionLogic(object sender, RoutedEventArgs e)
         {
             int intput1 = 0;
             int intput2 = 0;
             int.TryParse(input1.Text, out intput1);
             int.TryParse(input2.Text, out intput2);
             var button = sender as Button;
-            Answer.Content = intput1 + intput2;
+            Answer.Content = intput1 - intput2;
         }
 
-        private void subtraction(object sender, RoutedEventArgs e)
+        private void multiplyLogic(object sender, RoutedEventArgs e)
         {
             int intput1 = 0;
             int intput2 = 0;
             int.TryParse(input1.Text, out intput1);
             int.TryParse(input2.Text, out intput2);
             var button = sender as Button;
-            Answer.Content = intput1 + intput2;
+            Answer.Content = intput1 * intput2;
         }
 
-        private void multiply(object sender, RoutedEventArgs e)
+        private void divideLogic(object sender, RoutedEventArgs e)
         {
+            int intput1 = 0;
+            int intput2 = 0;
+            int.TryParse(input1.Text, out intput1);
+            int.TryParse(input2.Text, out intput2);
             var button = sender as Button;
+            Answer.Content = intput1 / intput2;
         }
 
-        private void divide(object sender, RoutedEventArgs e)
+        private void moduloLogic(object sender, RoutedEventArgs e)
         {
+            int intput1 = 0;
+            int intput2 = 0;
+            int.TryParse(input1.Text, out intput1);
+            int.TryParse(input2.Text, out intput2);
             var button = sender as Button;
-        }
-
-        private void modulo(object sender, RoutedEventArgs e)
-        {
-            var button = sender as Button;
+            Answer.Content = intput1 % intput2;
         }
     }
 }
