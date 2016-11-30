282cda7a7e908a293930f38b8b9334b72183283a
Angel Murchison <angelmurchison09@gmail.com>
2016-11-30 15:29:17 -0500

    Created xaml.cs logic for addition.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,14 +20,45 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void addition(object sender, RoutedEventArgs e)
         {
+            int intput1 = 0;
+            int intput2 = 0;
+            int.TryParse(input1.Text, out intput1);
+            int.TryParse(input2.Text, out intput2);
+            var button = sender as Button;
+            Answer.Content = intput1 + intput2;
+        }
 
+        private void subtraction(object sender, RoutedEventArgs e)
+        {
+            int intput1 = 0;
+            int intput2 = 0;
+            int.TryParse(input1.Text, out intput1);
+            int.TryParse(input2.Text, out intput2);
+            var button = sender as Button;
+            Answer.Content = intput1 + intput2;
+        }
+
+        private void multiply(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+        }
+
+        private void divide(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+        }
+
+        private void modulo(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
         }
     }
 }
